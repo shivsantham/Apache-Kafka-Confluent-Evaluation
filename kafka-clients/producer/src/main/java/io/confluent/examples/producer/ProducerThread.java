13e3bf219c81df3ac9f92800592540944023cb70
Siva Santhalingam <siva.santhalingam@gmail.com>
2016-08-28 17:53:13 -0700

    Update ProducerThread.java

--- kafka-clients/producer/src/main/java/io/confluent/examples/producer/ProducerThread.java
+++ kafka-clients/producer/src/main/java/io/confluent/examples/producer/ProducerThread.java
@@ -13,7 +13,6 @@ import java.util.Properties;
 import java.util.TimeZone;
 import java.util.Map;
 import java.util.HashMap;
-//import java.util.concurrent.ConcurrentHashMap;; 
 
 public class ProducerThread implements Runnable {
 
@@ -23,33 +22,26 @@ public class ProducerThread implements Runnable {
 	private Properties properties;
 	private Long threadno;
 	private int numMessages;
-	//private static long count = 0;
 	private static Map<String,Long> topicCount = new HashMap<String,Long>();
-	
+	private static Producer<String, String> producer = null;
+
 	static Object lock = new Object();
 	java.util.Date date;
-	//private static Map<String,Long> syncmap = new HashMap<String,Long>(); 
-	
-	ProducerThread(String topicName,String key, String datatoSend,
-                   int numMessages, Properties properties){
-        this.topicName = topicName;
-        this.key = key;
-        this.datatoSend = datatoSend;
-        this.properties = properties;
-        this.numMessages = numMessages;
-        date= new java.util.Date();
-        
-        synchronized(lock){
-        if(topicCount.get(topicName) == null){
-           
-        	topicCount.put(topicName,(long) 0);	
-        }
-        }
-        
 
-        //this.threadno= thread;
-    }
-    private static Producer<String, String> producer = null;
+	ProducerThread (String topicName,String key, String datatoSend, int numMessages, Properties properties) {
+        	this.topicName = topicName;
+        	this.key = key;
+        	this.datatoSend = datatoSend;
+        	this.properties = properties;
+        	this.numMessages = numMessages;
+        	date = new java.util.Date();
+        
+        	synchronized(lock){
+        		if(topicCount.get(topicName) == null){
+           		   topicCount.put(topicName,(long) 0);	
+        		}
+        	}
+        }
     
 
     public void run(){
@@ -57,21 +49,11 @@ public class ProducerThread implements Runnable {
       sendRecords(topicName,key,datatoSend,numMessages,producer);
     }
 
-  
-  /*  public static long getTopicCount(String topic){
-        if(syncmap.containsKey(topic)){
-           return syncmap.get(topic);
-        }
-        else 
-        return -1;
-    }*/
-    
     public static long getTopicCount(String topic){
         synchronized(lock) {
         return topicCount.get(topic);
         }
     }
-    
     
     public void sendRecords(String topicName,String key, String dataJSONString, int numMessages,
                         Producer<String, String> producer){
@@ -81,29 +63,18 @@ public class ProducerThread implements Runnable {
          
         Long startThreadTime = System.currentTimeMillis();
          
-             for(int i=0; i<numMessages;i++){
-                 ProducerRecord<String, String> data = new ProducerRecord<String, String>(
+        for (int i=0; i<numMessages;i++) {
+             ProducerRecord<String, String> data = new ProducerRecord<String, String>(
                        topicName, key, dataJSONString);
-                 producer.send(data);
+             producer.send(data);
                  synchronized(lock) {
-                     //count++;
                       topicCount.put(topicName,topicCount.get(topicName) + 1);
-                     //syncmap.put(topicName, count);
                  }
-             }
-         
-         long produceThreadTime = System.currentTimeMillis();
-         long elapsedTime = produceThreadTime - startThreadTime ;
-         
-         /*System.out.println(new Timestamp(date.getTime()) +
-                    " :] Time taken to publish " + dataJSONString.length() +
-                    " characters ::" + numMessages +":: times on Topic "+ topicName+ 
-                    "for producer Thread" + Thread.currentThread() + 
-                    " ::: " + elapsedTime + " milli seconds");*/
-         
-         ProducerGroup.totalTimeProducing += elapsedTime;
+        }
+        long produceThreadTime = System.currentTimeMillis();
+        long elapsedTime = produceThreadTime - startThreadTime ;
+        ProducerGroup.totalTimeProducing += elapsedTime;
      } 
-     
      catch (Exception e) {
             e.printStackTrace();
             returnStatus = false;
